da236269e264c685faa79853a00f918dd10a0799
unknown <wlhmyit@126.com>
2014-07-21 22:54:13 +0800

    add ajax post register

--- 5cm/rest/rest.js
+++ 5cm/rest/rest.js
@@ -2,6 +2,6 @@ var UserRest = require('./userRest');
 
 
 module.exports = function(app){
-	// app.post('/user/create', UserRest.create); 暂时不能暴露因为没有短信获取验证码
+	app.post('/user/create', UserRest.create); //暂时不能暴露因为没有短信获取验证码
 	app.post('/user/get', UserRest.get);
 };
