9f5f5b851d62ffb865b202a7aa49570795856387
wlh <lihua.wang@autonavi.com>
2014-07-22 10:24:37 +0800

    add rest single instance

--- 5cm/rest/rest.js
+++ 5cm/rest/rest.js
@@ -2,6 +2,6 @@ var UserRest = require('./userRest');
 
 
 module.exports = function(app){
-	app.post('/user/create', UserRest.create); //暂时不能暴露因为没有短信获取验证码
+	app.post('/user/create', UserRest.create); //注册接口中包含验证码功能的实例（单例）
 	app.post('/user/get', UserRest.get);
 };
