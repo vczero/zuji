a75ec843ecb7e4df0e3a35e83c20abc49558fa1b
wlh <wlhmyit@126.com>
2014-08-24 17:04:15 +0800

    add

--- 5cm/webapp/tpl/header.js
+++ 5cm/webapp/tpl/header.js
@@ -1,4 +1,9 @@
-/*TMODJS:{"version":7,"md5":"06d34184954351be4436fcf434869e5a"}*/
+/*TMODJS:{"version":14,"md5":"c1e610e03f26520eb055c62f112a3547"}*/
 define(function(require) {
-    return require("./template")("header", '<header> <div class="header"> <div id="header_back" class="cursor_po"></div> <div id="header_title" class="cursor_po"><%=title%></div> </div> </header> ');
+    return require("./template")("header", function($data) {
+        "use strict";
+        var $utils = this, $escape = ($utils.$helpers, $utils.$escape), title = $data.title, $out = "";
+        return $out += ' <header> <div class="header"> <div id="header_back" class="cursor_po"></div> <div id="header_title" class="cursor_po">', 
+        $out += $escape(title), $out += "</div> </div> </header> ", new String($out);
+    });
 });
